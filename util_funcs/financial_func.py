aae993e8dc4b2ef230f5ee24c92af6dcf7f8b611
Gautam <30121546+gautam-b@users.noreply.github.com>
2020-05-09 10:32:55 +0530

    formating changes

--- util_funcs/financial_func.py
+++ util_funcs/financial_func.py
@@ -17,11 +17,11 @@ def secant_method(tol, f, x0):
     x0: a starting value of x to begin the solver
     Notes
     ------
-    The secant method for finding the zero value of a function uses the following formula to find subsequent values of x. 
+    The secant method for finding the zero value of a function uses the following formula to find subsequent values of x.
 
     x(n+1) = x(n) - f(x(n))*(x(n)-x(n-1))/(f(x(n))-f(x(n-1)))
-    
-    Warning 
+
+    Warning
     --------
     This implementation is simple and does not handle cases where there is no solution. Users requiring a more robust version should use scipy package optimize.newton.
     """
@@ -45,8 +45,8 @@ def xnpv(rate, cashflows):
     * returns a single value which is the NPV of the given cash flows.
     Notes
     ---------------
-    * The Net Present Value is the sum of each of cash flows discounted back to the date of the first cash flow. The discounted value of a given cash flow is A/(1+r)**(t-t0), where A is the amount, r is the discout rate, and (t-t0) is the time in years from the date of the first cash flow in the series (t0) to the date of the cash flow being added to the sum (t).  
-    * This function is equivalent to the Microsoft Excel function of the same name. 
+    * The Net Present Value is the sum of each of cash flows discounted back to the date of the first cash flow. The discounted value of a given cash flow is A/(1+r)**(t-t0), where A is the amount, r is the discout rate, and (t-t0) is the time in years from the date of the first cash flow in the series (t0) to the date of the cash flow being added to the sum (t).
+    * This function is equivalent to the Microsoft Excel function of the same name.
     """
 
     chron_order = sorted(cashflows, key=lambda x: x[0])
@@ -61,14 +61,14 @@ def xirr(cashflows, guess=0.1):
     Arguments
     ---------
     * cashflows: a list object in which each element is a tuple of the form (date, amount), where date is a python datetime.date object and amount is an integer or floating point number. Cash outflows (investments) are represented with negative amounts, and cash inflows (returns) are positive amounts.
-    * guess (optional, default = 0.1): a guess at the solution to be used as a starting point for the numerical solution. 
+    * guess (optional, default = 0.1): a guess at the solution to be used as a starting point for the numerical solution.
     Returns
     --------
     * Returns the IRR as a single value
 
     Notes
     ----------------
-    * The Internal Rate of Return (IRR) is the discount rate at which the Net Present Value (NPV) of a series of cash flows is equal to zero. The NPV of the series of cash flows is determined using the xnpv function in this module. The discount rate at which NPV equals zero is found using the secant method of numerical solution. 
+    * The Internal Rate of Return (IRR) is the discount rate at which the Net Present Value (NPV) of a series of cash flows is equal to zero. The NPV of the series of cash flows is determined using the xnpv function in this module. The discount rate at which NPV equals zero is found using the secant method of numerical solution.
     * This function is equivalent to the Microsoft Excel function of the same name.
     * For users that do not have the scipy module installed, there is an alternate version (commented out) that uses the secant_method function defined in the module rather than the scipy.optimize module's numerical solver. Both use the same method of calculation so there should be no difference in performance, but the secant_method function does not fail gracefully in cases where there is no solution, so the scipy.optimize.newton version is preferred.
     """
