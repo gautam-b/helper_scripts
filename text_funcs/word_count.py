089925ceb2cd0beef22b748daa4a08d9c76f028d
Gautam <30121546+gautam-b@users.noreply.github.com>
2020-05-10 12:17:10 +0530

    merged functions to single file

--- text_funcs/word_count.py
+++ text_funcs/word_count.py
@@ -1,5 +1,5 @@
 """
-Count the frequency of each word in a string and return the sorted dict
+Count the frequency of each char / word in a string
 """
 
 from typing import Mapping
@@ -7,6 +7,10 @@ from nltk import word_tokenize
 from nltk.corpus import stopwords
 from string import punctuation
 from collections import Counter
+
+
+def char_count(text: str) -> Mapping[str, int]:
+    return Counter(text)
 
 
 def word_count(sent: str, filter_stopwords: bool = False) -> Mapping[str, int]:
@@ -19,6 +23,28 @@ def word_count(sent: str, filter_stopwords: bool = False) -> Mapping[str, int]:
 
 
 if __name__ == "__main__":
-    sent = "The quick brown fox jumps over the lazy dog."
-    print(word_count(sent))
-    print(word_count(sent, filter_stopwords=True))
+    text = '''
+    Her old collecting she considered discovered. So at parties he warrant oh staying. Square new horses and put better end. Sincerity collected happiness do is contented. Sigh ever way now many. Alteration you any nor unsatiable diminution reasonable companions shy partiality. Leaf by left deal mile oh if easy. Added woman first get led joy not early jokes. 
+
+In friendship diminution instrument so. Son sure paid door with say them. Two among sir sorry men court. Estimable ye situation suspicion he delighted an happiness discovery. Fact are size cold why had part. If believing or sweetness otherwise in we forfeited. Tolerably an unwilling arranging of determine. Beyond rather sooner so if up wishes or. 
+
+Is we miles ready he might going. Own books built put civil fully blind fanny. Projection appearance at of admiration no. As he totally cousins warrant besides ashamed do. Therefore by applauded acuteness supported affection it. Except had sex limits county enough the figure former add. Do sang my he next mr soon. It merely waited do unable. 
+
+Feet evil to hold long he open knew an no. Apartments occasional boisterous as solicitude to introduced. Or fifteen covered we enjoyed demesne is in prepare. In stimulated my everything it literature. Greatly explain attempt perhaps in feeling he. House men taste bed not drawn joy. Through enquire however do equally herself at. Greatly way old may you present improve. Wishing the feeling village him musical. 
+
+Sex reached suppose our whether. Oh really by an manner sister so. One sportsman tolerably him extensive put she immediate. He abroad of cannot looked in. Continuing interested ten stimulated prosperous frequently all boisterous nay. Of oh really he extent horses wicket. 
+
+Repulsive questions contented him few extensive supported. Of remarkably thoroughly he appearance in. Supposing tolerably applauded or of be. Suffering unfeeling so objection agreeable allowance me of. Ask within entire season sex common far who family. As be valley warmth assure on. Park girl they rich hour new well way you. Face ye be me been room we sons fond. 
+
+As am hastily invited settled at limited civilly fortune me. Really spring in extent an by. Judge but built gay party world. Of so am he remember although required. Bachelor unpacked be advanced at. Confined in declared marianne is vicinity. 
+
+Attended no do thoughts me on dissuade scarcely. Own are pretty spring suffer old denote his. By proposal speedily mr striking am. But attention sex questions applauded how happiness. To travelling occasional at oh sympathize prosperous. His merit end means widow songs linen known. Supplied ten speaking age you new securing striking extended occasion. Sang put paid away joy into six her. 
+
+Sportsman do offending supported extremity breakfast by listening. Decisively advantages nor expression unpleasing she led met. Estate was tended ten boy nearer seemed. As so seeing latter he should thirty whence. Steepest speaking up attended it as. Made neat an on be gave show snug tore. 
+
+Style too own civil out along. Perfectly offending attempted add arranging age gentleman concluded. Get who uncommonly our expression ten increasing considered occasional travelling. Ever read tell year give may men call its. Piqued son turned fat income played end wicket. To do noisy downs round an happy books. 
+
+'''
+    # sent = "The quick brown fox jumps over the lazy dog."
+    print(word_count(text))
+    print(word_count(text, filter_stopwords=True))
